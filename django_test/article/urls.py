ce1b5a09dcf5f0e76e7f97e9456d773604d233a6
chanish-teramatrix <chanish.agarwal@teramatrix.in>
2015-09-17 15:20:03 +0530

    till cookies and session

--- django_test/article/urls.py
+++ django_test/article/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import include, url
 urlpatterns = [
 	url(r'^all/$', 'article.views.articles'),
 	url(r'^get/(?P<article_id>\d+)/$', 'article.views.article'),
+	url(r'^language/(?P<language>[a-z\-]+)/$', 'article.views.language'),
 ]
--- django_test/article/views.py
+++ django_test/article/views.py
@@ -32,8 +32,19 @@ class HelloTemplate(TemplateView):
 
 
 def articles(request):
+	language = 'en-us' # stores in cookies
+	session_language = 'en-us' # stores in session
+
+	if 'lang' in request.COOKIES:	
+		language = request.COOKIES['lang']
+
+	if 'lang' in request.session:
+		session_language = request.session['lang']
+
 	return render_to_response('articles.html',
-	  						   {'articles': Article.objects.all()})
+	  						   {'articles': Article.objects.all(),
+	  						   'language': language,
+	  						   'session_language': session_language})
 
 #article_id = 1 has been choosen as default value.
 def article(request, article_id = 1):
@@ -42,3 +53,11 @@ def article(request, article_id = 1):
 
 
 
+def language(request, language='en-us'):
+	responce = HttpResponse('setting language to %s' %language)
+
+	responce.set_cookie('lang',language)
+
+	request.session['lang'] = language
+ 
+	return responce
